7c494750cc539249b664bc238e295dc5aabb0b14
Rick <rickoliwer@gmail.com>
2021-04-14 10:00:25 +0200

    move theme to repo

---
+++ category.php
@@ -0,0 +1,45 @@
+<?php 
+
+get_header();
+
+?>
+
+    <h1>Hello, world! & Category</h1>
+
+    <hr />
+
+    <!-- do we have any posts to display? -->
+    <?php if(have_posts()) : ?>
+        <!-- heh we have posts to display -->
+        <?php while(have_posts()) : ?>
+
+            <?php 
+                the_post(); 
+            ?>
+            <article class="card">
+                
+                <div class="card-body">
+                    
+                    <h2 class="card-title">
+                        <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                        </a>
+                    </h2>
+                    <div class="card-meta text-muted small mb-2">
+                        post published <?php the_date(); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>
+                    </div>
+                    
+
+                    <p class="card-text">
+                        <?php the_excerpt(); ?>
+                    </p>
+                
+                </div>
+            
+            </article>
+        <?php endwhile ; ?>
+    <?php endif ; ?>
+
+<?php 
+
+get_footer();
