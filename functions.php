ff0a57de1ffbe058c863b78a5412a210ccff0730
Rick <rickoliwer@gmail.com>
2021-04-14 13:07:44 +0200

    implement widget and menus in functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,59 @@
 <?php
 
-
+/**
+ * Change length of auto-generated excerpt.
+ *
+ * @param int $length
+ * @return int
+ */
 function mybasictheme_excerpt_length($length) {
-    return 10;
+	return 10;
 }
 add_filter('excerpt_length', 'mybasictheme_excerpt_length', 10, 1);
 
-function mybasictheme_excerpt_length_again($length) {
-    return 5;
+/**
+ * Change suffix on auto-generated excerpt.
+ *
+ * @param string $suffix
+ * @return string
+ */
+function mbt_filter_excerpt_more($suffix) {
+	return "...";
+}
+add_filter('excerpt_more', 'mbt_filter_excerpt_more');
+
+/**
+ * Append a 'Read more'-button to excerpts.
+ *
+ * @param string $excerpt
+ * @return string
+ */
+function mbt_filter_the_excerpt($excerpt) {
+	return $excerpt . '<div><a href="' . get_the_permalink() . '" class="btn btn-primary">Read more &raquo;</a></div>';
+}
+add_filter('the_excerpt', 'mbt_filter_the_excerpt');
+
+/**
+ * Declare support for 'title-tag'
+ */
+add_theme_support('title-tag');
+
+/**
+ * register widfet areas (a.k.a sidebars).
+ * 
+ * @return void
+ */
+
+function mbt_widgets_init(){
+    register_sidebar([
+        'name' => 'Blog sidebar',
+        'id' => 'blog-sidebar',
+        'description' => '',
+        'before_widget' => '<aside id="%1$s" class="card mb-3 widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title h5">',
+        'after_title' => '</h3>',
+    ]);
 }
 
-add_filter('excerpt_length', 'mybasictheme_excerpt_length_again', 10, 1);
+add_action('widgets_init', 'mbt_widgets_init');
